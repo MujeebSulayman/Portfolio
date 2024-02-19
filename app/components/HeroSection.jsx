37f4aa70fba9ff4f43ef6fe89aee623878352946
Mujeeb Sulayman <sulaymanmujeeb6@Gmail.com>
2024-02-19 12:01:29 +0100

    social..

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
 import { TypeAnimation } from 'react-type-animation';
+import Link from 'next/link';
 
 const HeroSection = () => {
 	const [animate, setAnimate] = useState(false);
@@ -44,12 +45,18 @@ const HeroSection = () => {
 						leave a lasting impression and deliver seamless digital experiences.
 					</p>
 					<div>
-						<button className='px-6 py-3 font-sans rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-400 to-pink-600 hover:bg-slate-200 text-gray-100'>
+						<Link
+							href='https://www.linkedin.com/in/sulayman-abdul-mujeeb-299909214/'
+							className='px-6 inline-block py-3 w-full sm:w-fit rounded-md mr-4 font-sans font-semibold bg-gray-400 hover:bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-gray-500 text-gray-900 hover:text-white'>
 							Hire Me
-						</button>
-						<button className='px-6 py-3 font-sans rounded-full bg-gradient-to-br from-blue-500 via-purple-500 hover:bg-slate-800 text-gray-100 border mt-3'>
-							Download CV
-						</button>
+						</Link>
+						<Link
+							href='https://drive.google.com/file/d/16VjjJ34UrzSKr7nzsfo8ATw8DNTSt1xI/view?usp=sharing'
+							className='px-1 inline-block py-1 w-full sm:w-fit rounded-md font-sans font-semibold bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3'>
+							<span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
+								Download CV
+							</span>
+						</Link>
 					</div>
 				</div>
 				<div className='col-span-5 place-self-center'>
